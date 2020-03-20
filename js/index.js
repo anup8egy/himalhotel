94acbd9583bf39d1a7266826e0d5b934dd86610c
anup8egy <anup8eguy@gmail.com>
2020-03-20 21:53:58 +0545

    seocnd Commit

--- js/index.js
+++ js/index.js
@@ -1,7 +1,18 @@
+let c=0;
 $(document).ready(function(){
     $('.navButt').on('click',showHideMobileNav);
 })
-function showHideMobileNav(){
+function showHideMobileNav(){//To open popup for menu bar in mobile
+    c+=1;
     $('.mobNavList').fadeToggle();
-    $('.navButt i').toggleClass('fa-close');
+    if(c%2==1){
+        var check=true;
+        $('.mobNavList').show();
+        gsap.to('.mobNavList',{ duration: 2.5, ease: "expo.in"});
+    }else{
+        var check=false;
+        $('.mobNavList').fadeOut();
+    }
+    $('.navButt i').toggleClass('fa-bars',!check);
+    $('.navButt i').toggleClass('fa-close',check);
 }
